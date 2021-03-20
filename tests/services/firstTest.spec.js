27535c4cffebb2a4fc3b8085777605aaabc06ada
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2021-03-20 16:14:42 -0300

    simulating a broken test

--- tests/services/firstTest.spec.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-describe("only for testing purposes", function () {
-    it("should be able to pass a test", function () {
-        expect(1 + 2).toBe(3);
-    });
-});
--- tests/services/firstTest.spec.ts
+++ tests/services/firstTest.spec.ts
@@ -1,5 +1,5 @@
 describe("only for testing purposes", () => {
   it("should be able to pass a test", () => {
-    expect(1 + 2).toBe(3);
+    expect(1 + 2).toBe(4);
   });
 });
