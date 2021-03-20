4df83b833078148be1402ffe916cec89ccf2dd6b
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2021-03-20 15:38:31 -0300

    db config

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,12 +3,12 @@ import { createConnection, getConnectionOptions } from "typeorm";
 export default async function setupDatabase(): Promise<void> {
   const defaultOptions = await getConnectionOptions();
 
-  // if (process.env.NODE_ENV !== "development") {
-  //   console.log(process.env.DATABASE_URL);
-  //   Object.assign(defaultOptions, {
-  //     url: `${process.env.DATABASE_URL}?ssl:true`,
-  //   });
-  // }
+  if (process.env.NODE_ENV !== "development") {
+    console.log(process.env.DATABASE_URL);
+    Object.assign(defaultOptions, {
+      ssl: true,
+    });
+  }
 
   createConnection(defaultOptions)
     .then(() => {
