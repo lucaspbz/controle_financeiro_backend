6c84b9a4aa9c8e0414665a976ae0d1307ced27d3
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2021-03-20 14:46:15 -0300

    db config

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   // port: `${process.env.DB_PORT}`,
   // username: `${process.env.DB_USER}`,
   // password: `${process.env.DB_PASSWORD}`,
-  // database: `${process.env.DB_NAME}`,
+  database: 'controle_financeiro',
   url: process.env.DATABASE_URL,
   schema: process.env.NODE_ENV === 'test' ? 'test' : 'public',
   entities:
