1a3f365ae59f20385a5756e2120e9827112e0ea0
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2021-03-20 14:40:51 -0300

    ci config

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   type: 'postgres',
-  host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
-  username: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`,
+  // host: `${process.env.DB_HOST}`,
+  // port: `${process.env.DB_PORT}`,
+  // username: `${process.env.DB_USER}`,
+  // password: `${process.env.DB_PASSWORD}`,
+  // database: `${process.env.DB_NAME}`,
+  url: process.env.DATABASE_URL,
   schema: process.env.NODE_ENV === 'test' ? 'test' : 'public',
   entities:
     process.env.NODE_ENV === 'development'
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,11 @@ import { createConnection, getConnectionOptions } from "typeorm";
 
 export default async function setupDatabase(): Promise<void> {
   const defaultOptions = await getConnectionOptions();
+
+  if (process.env.NODE_ENV === "development") {
+    Object.assign(defaultOptions, { url: process.env.DATABASE_URL });
+  }
+
   createConnection(defaultOptions)
     .then(() => {
       console.log(`Database connected!`);
